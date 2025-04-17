5318772485ddd8929635fdc68645b1673b9a68cb
AndriX05 <vociandripersonal@gmail.com>
2025-04-17 18:52:22 +0200

    Add files via upload

---
+++ workout-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorkoutApp from './WorkoutApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorkoutApp />
+  </React.StrictMode>,
+)
